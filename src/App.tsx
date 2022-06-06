03b8aa1db843dd738cc9fcaf9b73cf69a7dac625
ferdous-al-quraeshi <ecopavel@gmail.com>
2022-06-06 21:43:15 +0600

    feat: sider with custom trigger and color

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import logo from './logo-sneat.png'
+import '/node_modules/antd/dist/antd.css';
 import './App.css';
 
-function App() {
+import { Layout, Menu } from 'antd';
+import {
+  LeftCircleFilled,
+  RightCircleFilled,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        {React.createElement(collapsed ? RightCircleFilled : LeftCircleFilled, {
+          className: 'trigger',
+          style: {fontSize: 24, color: '#696cff'},
+          onClick: () => setCollapsed(!collapsed),
+        })}
+        <div className="logo">
+          {collapsed
+            ? <img src={logo} alt="logo" height={24} width='auto' />
+            : <><img src={logo} alt="logo" height={24} width='auto' />            
+              <span>xpense</span></>
+          }
+        </div>
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ margin: 15 }}>
+          
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
